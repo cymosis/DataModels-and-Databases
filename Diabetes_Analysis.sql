273fc0bd0a031fcde45183e077b1246fefae8696
Cynthia Mutisya <97816621+cymosis@users.noreply.github.com>
2025-03-27 21:58:59 +0100

    Update Diabetes_Analysis.sql

--- Diabetes_Analysis.sql
+++ Diabetes_Analysis.sql
@@ -2,6 +2,7 @@
 select diabetes_description,count(*)as ROW_COUNT from diabetes_transformed group by diabetes_description;
 
 --Descriptives of diabetes based on other input factors
-select sex_description,diabetes_description,count(*) from diabetes_transformed 
+select round(avg(BMI),2) as average_bmi,sex_description,diabetes_description,count(*) from diabetes_transformed 
 where diabetes_description <> 'No diabetes' AND BMI > (select avg(BMI)from diabetes_transformed) 
-group by sex_description,diabetes_description;
+group by sex_description,diabetes_description having avg(bmi) > (select avg(bmi) from diabetes_transformed);
+
